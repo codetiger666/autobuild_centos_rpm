2b38e01c22c41aaac7406292a43590e0052ef022
yc <yc@111179.xyz>
2025-01-10 16:30:15 +0800

    添加统一编译脚本

--- scripts/openresty.sh
+++ scripts/openresty.sh
@@ -13,6 +13,6 @@ program_init(){
   sudo /bin/cp nginx.conf rpm/rpmbuild/SOURCES
   sudo sed -i "s/codetiger_version/${project_program}/g" specs/openresty.spec
   sudo sed -i "s/codetiger_perl_version/${PERLVERSION}/g" specs/openresty.spec
-  wget https://openresty.org/download/openresty-$OPENRESTYVERSION.tar.gz
-  sudo /bin/cp openresty-$OPENRESTYVERSION.tar.gz rpm/rpmbuild/SOURCES/
+  wget https://openresty.org/download/openresty-${project_version}.tar.gz
+  sudo /bin/cp openresty-${project_version}.tar.gz rpm/rpmbuild/SOURCES/
 }
